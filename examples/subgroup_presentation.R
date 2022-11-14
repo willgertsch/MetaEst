3be821352817707bd3a2541029d1b0990e6d0f42
willgertsch <willgertsch+github@gmail.com>
2022-11-14 11:58:44 -0800

    simulation study code ready to run

--- examples/subgroup_presentation.R
+++ examples/subgroup_presentation.R
@@ -29,3 +29,48 @@ plot(density(df[df$treat==1,]$Y))
 
 mod = lm(Y ~ sex * treat, df)
 summary(mod)
+
+
+# process results of simulation study
+sim_data = read.csv("examples/subgroup_simulation_study.csv") %>%
+as.data.frame() %>%
+filter(method != "EM")
+
+# rename columns
+colnames(sim_data) = c("method", "loglik", "beta11", "beta12",
+"beta21", "beta22", "gamma1", "gamma2", "sigma", "n")
+
+rmse = function(theta, thetahat) {
+    e = theta - thetahat
+    e2 = e^2
+    mse = sum(e2)/length(thetahat)
+    return(sqrt(mse))
+}
+
+# calculate summary statistics
+sim_data_sum = sim_data %>%
+group_by(method, n) %>%
+summarise(
+    median_ll = median(loglik),
+    beta11_rmse = rmse(80, beta11),
+    beta12_rmse = rmse(0, beta12),
+    beta21_rmse = rmse(0, beta21),
+    beta22_rmse = rmse(30, beta22),
+    gamma1_rmse = rmse(-1.39, gamma1),
+    gamma2_rmse = rmse(1.79, gamma2),
+    sigma_rmse = rmse(1, sigma)
+)
+sim_data_sum
+
+# plots for interesting parameters
+# log-like
+ggplot(sim_data_sum, aes(x = n, y = median_ll, color = method)) +
+geom_point() + geom_line()
+
+# treament effect null group RMSE
+ggplot(sim_data_sum, aes(x = n, y = beta12_rmse, color = method)) +
+geom_point() + geom_line()
+
+# interaction effect
+ggplot(sim_data_sum, aes(x = n, y = beta22_rmse, color = method)) +
+geom_point() + geom_line()
--- examples/subgroup_presentation.jl
+++ examples/subgroup_presentation.jl
@@ -80,3 +80,80 @@ df = DataFrame(results, ["loglik", "β₁₁", "β₁₂", "β₂₁", "β₂₂
 df.method = ["ECA", "DE", "PSO", "SA", "WOA", "GA", "εDE"];
 select!(df, :method, :); # reorder columns
 sort!(df, :loglik, rev = true)
+
+# simulation study comparing different algorithms
+# and at different sample sizes
+Nsims = 10 # run 1000 simulations
+sample_sizes = [30, 50, 100, 300, 800]
+#sample_sizes = [30, 50]
+simulation_results = Vector{Matrix{Float64}}(undef, Nsims * length(sample_sizes));
+for i in 1:Nsims * length(sample_sizes)
+    simulation_results[i] = Matrix{Float64}(undef, 8, 9)
+end
+
+# setting - parameter true values
+β₁ = [80., 0.] # regression parameters baseline
+β₂ = [0., 30.] # increases due to latent subgroup
+γ = [-1.39, 1.79] # rate of Gene X in women = .6, in men = .2
+σ = 1.
+
+
+# parallel loop
+Random.seed!(4321)
+# tool for putting data in results matrix
+for i in eachindex(sample_sizes)
+
+    N_i = sample_sizes[i]
+
+    println("Simulating for sample size ", N_i)
+
+    # simulate in parallel
+    Threads.@threads for j in 1:Nsims
+
+        # generate simulated data
+        # sample men and women with 3/2 ratio
+        sex = StatsBase.sample([0., 1.], Weights([.6, .4]), N_i);
+        X = hcat(ones(N_i), sex);
+        # sample Gene X data based on logistic regression model
+        geneX = rand.(Bernoulli.(ilogit.(X * γ)));
+
+        # assign half to treatment
+        treat = repeat([0., 1.], inner = Int(N_i/2));
+        Z = hcat(ones(N_i), treat);
+
+        # generate from the mixture distribution
+        Y = Vector{Float64}(undef, N_i)
+        for i in 1:N_i
+            if geneX[i] == 1.
+                μ = Z[i, :]' * (β₁ + β₂)
+                Y[i] = rand(Normal(μ, σ))
+            else
+                μ = Z[i, :]' * β₁
+                Y[i] = rand(Normal(μ, σ))
+            end
+        end
+
+        # fit all models to data
+        obs = LnmObs(Y, X, Z)
+        mod = LnmModel(obs)
+
+        index = (i*Nsims - Nsims + j)
+        #println(index)
+        simulation_results[index][:, 1:8] .= fit_all!(mod)
+        simulation_results[index][:, 9] .= N_i # save sample size
+    end
+end
+
+# process data
+# convert to data frame
+# first step concatenates all matrices in vector of matrices
+df = DataFrame(reduce(vcat, simulation_results), 
+["loglik", "β₁₁", "β₁₂", "β₂₁", "β₂₂", "γ₁", "γ₂", "σ", "n"]);
+
+# create methods column
+df.method = repeat(["ECA", "DE", "PSO", "SA", "WOA", "GA", "εDE", "EM"], Nsims*length(sample_sizes));
+select!(df, :method, :); # reorder columns
+
+# write to CSV for analysis in R
+using CSV
+CSV.write("examples/subgroup_simulation_study.csv", df)
